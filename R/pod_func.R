8414a4b74dc729f7e001a89f53719f2f966efe06
Augustine Wigle <augustine.wigle@gmail.com>
2024-12-06 15:08:51 -0500

    fix typo in POD function

--- R/pod_func.R
+++ R/pod_func.R
@@ -6,10 +6,11 @@
 #' @export
 pod_func <- function(Ytrue, h, u, truncate = 0.01) {
 
-  ps <- exp(-1*((0.244*Ytrue^(1.07)/((h/1000)^(2.44)*(u+2.14)^(1.69)))^(-2.53)))
+  ps <- exp(-1*((0.224*Ytrue^(1.07)/((h/1000)^(2.44)*(u+2.14)^(1.69)))^(-2.53)))
 
   ps[which(ps < truncate)] <- truncate
 
   return(ps)
 
 }
+
